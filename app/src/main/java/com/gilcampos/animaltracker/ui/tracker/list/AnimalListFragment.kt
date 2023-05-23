e228cc674ad2fdfdcf9ed295394467feb883676b
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-05-23 10:54:26 -0600

    Finish adding the adapter to display the list of animals

--- app/src/main/java/com/gilcampos/animaltracker/ui/tracker/list/AnimalListFragment.kt
+++ app/src/main/java/com/gilcampos/animaltracker/ui/tracker/list/AnimalListFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.animaltracker.R
 import com.example.animaltracker.databinding.FragmentAnimalListBinding
+import com.gilcampos.animaltracker.model.Animal
 import com.gilcampos.animaltracker.ui.AnimalTrackerViewModel
 
 class AnimalListFragment : Fragment() {
@@ -15,6 +18,7 @@ class AnimalListFragment : Fragment() {
     private var _binding: FragmentAnimalListBinding? = null
     private val binding get() = _binding!!
     private val viewModel: AnimalTrackerViewModel by activityViewModels()
+    private lateinit var recyclerAdapter: AnimalListRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +26,41 @@ class AnimalListFragment : Fragment() {
     ): View? {
         _binding = FragmentAnimalListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpRecyclerView(view)
+        clickListeners()
+    }
+
+    private fun setUpRecyclerView(view: View) {
+        recyclerAdapter = AnimalListRecyclerAdapter {
+            listAnimalINfo(it)
+        }
+        binding.animalListRecyclerview.apply {
+            layoutManager = LinearLayoutManager(view.context)
+            adapter = recyclerAdapter
+        }
+        displayAnimals()
+    }
+
+    private fun displayAnimals() {
+        recyclerAdapter.setList(viewModel.getAnimals())
+        recyclerAdapter.notifyDataSetChanged()
+    }
+
+
+    private fun listAnimalINfo(animal: Animal) {
+        viewModel.selectedAnimal(animal)
+        findNavController().navigate(R.id.action_animalListFragment_to_animalDetailsFragment)
+    }
+
+    private fun clickListeners() {
+        binding.addNewAnimalFloatinButton.setOnClickListener {
+            viewModel.clearData()
+            findNavController().navigate(R.id.action_animalListFragment_to_addAnimalFragment)
+        }
     }
 
     override fun onDestroyView() {
